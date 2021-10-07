b2fea7730eacdcdc6b79e10a5aa787180e127ee7
David Petrizze <dpetrizze@gmail.com>
2021-10-07 12:56:13 -0600

    Move peer into its own module, separate rtt computation into its own submodule

--- src/peer/mod.rs
+++ src/peer/mod.rs
@@ -1,4 +1,6 @@
 
+mod rtt;
+
 use std::collections::VecDeque;
 use std::time;
 use std::ops::Range;
@@ -39,76 +41,6 @@ pub enum Event {
     Timeout,
 }
 
-struct Ping {
-    sequence_id: u16,
-    instant: time::Instant,
-}
-
-struct PingRtt {
-    ping_history: Vec<Ping>,
-    next_sequence_id: u16,
-    srtt_ms: f64,
-    sdrtt_ms: f64,
-    ping_received: bool,
-}
-
-impl PingRtt {
-    const DEFAULT_RTT_MS: f64 = 100.0;
-    const PING_TIMEOUT_MS: u64 = 3000;
-    const RTT_SMOOTH_ALPHA: f64 = 0.875;
-    const DRTT_SMOOTH_ALPHA: f64 = 0.875;
-    const RTO_SDRTT_U: f64 = 4.0;
-
-    pub fn new() -> Self {
-        Self {
-            ping_history: Vec::new(),
-            next_sequence_id: 0,
-            srtt_ms: Self::DEFAULT_RTT_MS,
-            sdrtt_ms: 0.0,
-            ping_received: false,
-        }
-    }
-
-    fn update_rtt(&mut self, rtt_ms: f64) {
-        if !self.ping_received {
-            self.ping_received = true;
-            self.srtt_ms = rtt_ms;
-            self.sdrtt_ms = 0.0;
-        } else {
-            self.srtt_ms = Self::RTT_SMOOTH_ALPHA*self.srtt_ms + (1.0 - Self::RTT_SMOOTH_ALPHA)*rtt_ms;
-            self.sdrtt_ms = Self::DRTT_SMOOTH_ALPHA*self.sdrtt_ms + (1.0 - Self::DRTT_SMOOTH_ALPHA)*(rtt_ms - self.srtt_ms).abs();
-        }
-    }
-
-    pub fn new_ping(&mut self, now: time::Instant) -> u16 {
-        let sequence_id = self.next_sequence_id;
-        self.ping_history.push(Ping { sequence_id: sequence_id, instant: now } );
-        self.next_sequence_id = self.next_sequence_id.wrapping_add(1);
-        sequence_id
-    }
-
-    pub fn handle_ack(&mut self, now: time::Instant, sequence_id: u16) {
-        for (idx, ping) in self.ping_history.iter().enumerate() {
-            if sequence_id == ping.sequence_id {
-                let rtt_ms = (now - ping.instant).as_secs_f64()*1_000.0;
-                self.ping_history.remove(idx);
-                self.update_rtt(rtt_ms);
-                break;
-            }
-        }
-
-        self.ping_history.retain(|ping| (now - ping.instant).as_millis() < Self::PING_TIMEOUT_MS as u128);
-    }
-
-    pub fn rtt_ms(&self) -> f64 {
-        self.srtt_ms
-    }
-
-    pub fn rto_ms(&self) -> f64 {
-        self.srtt_ms + Self::RTO_SDRTT_U*self.sdrtt_ms
-    }
-}
-
 const PING_INTERVAL_MS: u64 = 1000;
 const CONNECT_INTERVAL_MS: u64 = 500;
 const DISCONNECT_INTERVAL_MS: u64 = 500;
@@ -125,7 +57,7 @@ pub struct Peer {
     watchdog_time: time::Instant,
     meta_send_time: Option<time::Instant>,
 
-    ping_rtt: PingRtt,
+    ping_rtt: rtt::PingRtt,
 
     // Connect, Disconnect, Ping
     meta_queue: VecDeque<Box<[u8]>>,
@@ -154,7 +86,7 @@ impl Peer {
             watchdog_time: time::Instant::now(),
             meta_send_time: None,
 
-            ping_rtt: PingRtt::new(),
+            ping_rtt: rtt::PingRtt::new(),
 
             meta_queue: VecDeque::new(),
             event_queue: VecDeque::new(),
