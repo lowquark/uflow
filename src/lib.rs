3f6e4f4a1653df2324942de92a48373169b8da19
David Petrizze <dpetrizze@gmail.com>
2022-03-22 01:39:37 -0600

    Edit documentation

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,8 @@
 //! As a non-blocking socket interface, `Server` objects depend on periodic calls to
 //! [`step()`](Server::step) to process incoming UDP frames, and to update connection states
 //! accordingly. Similarly, [`flush()`](Server::flush) must be called in order to send pending
-//! outbound frames.
-//!
-//! Once a connection request has been received, a [`Peer`] object will be created to represent the
-//! connection. Newly connected peers are retrieved by calling [`incoming()`](Server::incoming()),
-//! from which the application is expected to maintain its own list of peers.
-//!
-//! A basic server loop that extends the above example is shown below:
+//! outbound data (packets, acknowledgements, keepalives, and etc.). A basic server loop that
+//! extends the above example is shown below:
 //!
 //! ```
 //! # let server_address = "127.0.0.1:8888";
@@ -68,6 +63,15 @@
 //! }
 //! ```
 //!
+//! #### Inbound Connections
+//!
+//! Once a remote host has connected to the server, a [`Peer`] object will be created to represent
+//! the connection. Newly created `Peer` objects may be retrieved by calling
+//! [`incoming()`](Server::incoming()), at which point they may be stored by the application
+//! wherever is most convenient.
+//!
+//! #### Example
+//!
 //! See the `echo_server` example for a complete server implementation.
 //!
 //! # Connecting to a Server
@@ -84,7 +88,7 @@
 //! ```
 //!
 //! A connection is then initiated by calling [`connect()`](Client::connect), which takes the
-//! address of the remote host, and the connection parameters to use. Assuming the provided address
+//! address of the remote host, and the endpoint parameters to use. Assuming the provided address
 //! could be resolved, a [`Peer`] object representing the connection will be returned.
 //!
 //! ```
@@ -124,15 +128,17 @@
 //! }
 //! ```
 //!
+//! #### Example
+//!
 //! See the `echo_client` example for a complete client implementation.
 //!
 //! # Sending Data
 //!
-//! Packets sent to a remote host by calling [`Peer::send()`], which takes a channel ID and a
-//! [`SendMode`] parameter. All packets received on a given channel are guaranteed to be delivered
-//! in the order they were sent, and each packet will be sent and resent according to the specified
-//! mode. Packets which are sent prior to establishing a connection will be sent once the
-//! connection succeeds.
+//! Packets are sent to a remote host by calling [`Peer::send()`], which additionally requires a
+//! channel ID and a packet send mode. All packets received on a given channel are guaranteed to be
+//! delivered in the order they were sent, and each packet will be transferred according to the
+//! [specified mode](SendMode). Packets which are sent prior to establishing a connection will be
+//! buffered until the connection succeeds.
 //!
 //! ```
 //! # let mut client = uflow::Client::bind_any_ipv4()
@@ -148,24 +154,32 @@
 //! peer.send(packet_data.into(), channel_id, send_mode);
 //! ```
 //!
-//! *Note 1*: All packets are aggregated into UDP frames, and are first fragmented if they are
-//! larger than [`MAX_FRAGMENT_SIZE`]. Packet data is not placed on the network until `flush()` is
-//! called on the associated client or server object.
+//! ##### Fragmentation
 //!
-//! *Note 2*: Because packets that are sent using [`SendMode::Reliable`] may not be skipped, the
-//! receiver will not deliver received packets for a given channel until all previous reliable
-//! packets on that channel have arrived. So, if a reliable packet is dropped, its channel will
-//! stall for its arrival, but packets received on other channels may still be delivered in the
-//! meantime. Careful selection of channel and send mode can thus mitigate the effects of
-//! short-term network congestion.
+//! All packets are aggregated into UDP frames, and are first fragmented if their size exceeds
+//! [`MAX_FRAGMENT_SIZE`]. No frames are sent until `flush()` is called on the associated client or
+//! server object.
 //!
-//! *Note 3*: All packets are sent subject to adaptive rate control, a memory limit set by the
-//! receiving host, and a maximum transfer window of [`MAX_PACKET_WINDOW_SIZE`] outstanding
-//! packets. If any of these mechanisms prevent a packet's delivery, the packet will remain in a
-//! send queue. (Packets sent using [`SendMode::TimeSensitive`] are an exception to this.) Thus, a
-//! sender can expect that packets will begin to accumulate in its send queue if the connection
-//! bandwidth is low, or if the receiver has stopped retrieving received packets (see
-//! [`poll_events()`](Peer::poll_events)). The application can query the total size of pending packets in the send queue by calling [`send_queue_size()`](Peer::send_queue_size).
+//! ##### Reliable Packets & Channel Stalls
+//!
+//! Because packets that are sent using [`SendMode::Reliable`] may not be skipped, and because all
+//! packets on a given channel must be delivered in-order, the application will not see received
+//! packets until all previous reliable packets on the same channel have been received. This means
+//! that if a reliable packet has been dropped, its channel will effectively stall for its arrival.
+//! However, packets received on other channels may still be delivered in the meantime. The effects
+//! of intermittent drops can otherwise be mitigated through careful selection of channel and send
+//! mode.
+//!
+//! ##### Packet Buffering
+//!
+//! All packets are sent subject to adaptive rate control, a maximum transfer window, and a memory
+//! limit set by the receiving host. If any of these mechanisms prevent a packet's delivery, the
+//! packet will remain in a send queue at the sender. (Packets sent using
+//! [`SendMode::TimeSensitive`] are an exception to this.) Thus, a sender can expect that packets
+//! will begin to accumulate in its send queue if the connection bandwidth is low, or if the
+//! receiver has ceased to call [`Peer::poll_events()`](Peer::poll_events). If this is a problem,
+//! the application may query the total size of pending packets in the send queue by calling
+//! [`Peer::send_queue_size()`](Peer::send_queue_size), and handle the situation accordingly.
 //!
 //! # Connection Events and Receiving Data
 //!
@@ -199,20 +213,22 @@
 //! [`Disconnect`](Event::Disconnect) event will be generated. Once a packet has been received in
 //! full (and that packet is not waiting for any previous packets), a [`Receive`](Event::Receive)
 //! event will be generated. If a connection times out at any point, a [`Timeout`](Event::Timeout)
-//! event will be generated.
+//! event will be generated. The last event generated by a `Peer` object will always be a
+//! `Timeout`, even after a connection has closed normally.
 //!
-//! *Note 1*: The last event generated by a `Peer` object will always be a `Timeout` event, even
-//! after a connection has closed normally.
+//! ##### Maximum Receive Allocation
 //!
-//! *Note 2*: Calls to `poll_events()` will free allocation space in the peer's receive buffer, and
-//! allow new packets to be received. A receiver can expect that if `poll_events()` is not called
-//! for whatever reason, that the number of packets in the receive buffer will be limited according
-//! to the endpoint's [maximum receive allocation](EndpointConfig#structfield.max_receive_alloc).
+//! If `poll_events()` is not called for whatever reason, the number of packets in the receive
+//! buffer will be limited according to the endpoint's [maximum receive
+//! allocation](EndpointConfig#structfield.max_receive_alloc), and the delivery of new packets will
+//! stall.
 //!
-//! *Note 3*: If desired, the application may call `(Client|Server)::flush()` just after all events
-//! have been handled for all peers. This will ensure that the most up-to-date acknowledgement
-//! information is sent as soon as possible. (If `flush()` was called directly after `step()`, the
-//! sent acknowledgements would not indicate which packets had been delivered. From a
+//! ##### Optimal Acknowledgements
+//!
+//! If desired, the application may call `flush()` on the associated client or server object
+//! immediately after all events have been handled, as this allows for the latest acknowledgement
+//! information to be sent as soon as possible. (If `flush()` were called prior to handling events,
+//! the acknowledgements sent would not indicate which packets had been delivered—from a
 //! synchronization standpoint, this is sub-optimal but non-fatal.)
 //!
 //! # Disconnecting
@@ -220,7 +236,7 @@
 //! A connection is explicitly closed by calling [`Peer::disconnect()`], which makes an effort to
 //! notify the remote host of the disconnection, and to send all pending outbound packets before
 //! doing so. The sender can expect that any pending reliable packets will be delivered prior to
-//! disconnecting, given the remote host doesn't also disconnect in the meantime.
+//! disconnecting, provided that the remote host doesn't also disconnect in the meantime.
 //!
 //! ```
 //! # let mut client = uflow::Client::bind_any_ipv4()
@@ -231,7 +247,7 @@
 //! #     .expect("Invalid address");
 //! peer.disconnect();
 //!
-//! // ... calls to step() continue
+//! // ... calls to step() & flush() continue
 //! ```
 //!
 //! Alternatively, one may call [`Peer::disconnect_now()`], which sends no further packets and
@@ -297,16 +313,16 @@ pub const MAX_PACKET_SIZE: usize = MAX_FRAGMENT_SIZE * frame::serial::MAX_FRAGME
 pub enum SendMode {
     /// This packet will be sent at most once. If this packet cannot be sent immediately (i.e.
     /// during the next call to [`Client::flush`](Client::flush) or
-    /// [`Server::flush`](Server::flush)), it will be discarded rather than remain in a send queue.
-    /// If this packet is dropped, or a subsequent packet arrives on the same channel before this
-    /// one does, the receiver may skip this packet.
+    /// [`Server::flush`](Server::flush)), it will be discarded instead of being delayed. If this
+    /// packet is dropped, or a subsequent packet arrives on the same channel before this one does,
+    /// the receiver may skip this packet.
     TimeSensitive,
     /// This packet will be sent exactly once. If this packet is dropped, or a subsequent packet
     /// arrives on the same channel before this one does, the receiver may skip this packet.
     Unreliable,
     /// This packet will be sent and resent until acknowledged by the receiver. If a subsequent
     /// packet arrives on the same channel before this one does, the receiver may skip this packet.
-    /// (In general, the packet will cease to be resent once the sender has detected a skip.)
+    /// (The packet will cease to be resent once the sender has detected a skip.)
     Persistent,
     /// This packet will be sent until acknowledged by the receiver. The receiver will not deliver
     /// subsequent packets on the same channel until this packet has been received.
